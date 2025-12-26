3f8a7142af2ab3377f57f8fc3dc4ad018a344b67
JANARDHAN NAIDU M <24331a05h7@mvgrce.edu.in>
2025-12-26 14:56:54 +0530

    Add files via upload

---
+++ min.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+minimum = min(a, b, c)
+maximum = max(a, b, c)
+print("Minimum number:", minimum)
+print("Maximum number:", maximum)
